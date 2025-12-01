888ea098311a5d03f69e229b62464bf670cb04d0
arun <arunachalamselvam9965@gmail.com>
2025-12-01 11:04:34 +0530

    comit

--- Main1.java
+++ Main1.java
@@ -1,6 +1,6 @@
 public class Main1 {
     public static void main(String[] args) {
-        System.out.println("Hello, Git!");
+        System.out.println("Hello, Git! and welcome to exploring repositories.");
         System.out.println("Exploring Git features.");
     }
 }
