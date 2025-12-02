52cca8c7056d16aa37de90db1c1e90383956fcf6
arun <arunachalamselvam9965@gmail.com>
2025-12-02 11:00:42 +0530

    Revert "Solved conflicts in Demo.java while merging DevelopmentBr"
    This reverts commit e6334de2ce85a1ed87f9f060fa9c98d3a902c8ad, reversing
    changes made to fcc5eda7b241fa7e12041ee90f7553157a94e58c.

--- Demo.java
+++ Demo.java
@@ -1,7 +1,5 @@
-
-class Demo{
+public class Demo {
     public static void main(String[] args) {
-        System.out.println("Hello, Git!");
-
+        System.out.println("This is a demo class for Git exploration.");
     }
 }
